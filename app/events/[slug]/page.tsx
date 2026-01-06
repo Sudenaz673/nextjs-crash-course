515f68f07a2231fa072e1f9abade372634d8a718
Sudenaz673 <sudenaz_muslu23@erdogan.edu.tr>
2026-01-07 02:28:27 +0300

    finalize the app

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import BookEvent from "@/components/BookEvent";
-import {IEvent} from "@/database";
-import {getSimilarEventsBySlug} from "@/lib/actions/event.actions";
+import { IEvent } from "@/database";
+import { getSimilarEventsBySlug } from "@/lib/actions/event.actions";
 import EventCard from "@/components/EventCard";
+import {cacheLife} from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const EventDetailItem = ({ icon, alt, label}: { icon: string; alt: string; label: string; }) => (
-<div className={"flex-row-gap-2 items-center"}>
-    <Image src={icon} alt={alt} width={17} height={17} />
-    <p>{label}</p>
-</div>
+const EventDetailItem = ({ icon, alt, label }: { icon: string; alt: string; label: string; }) => (
+    <div className={"flex-row-gap-2 items-center"}>
+        <Image src={icon} alt={alt} width={17} height={17} />
+        <p>{label}</p>
+    </div>
 )
 
 const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => (
@@ -23,10 +24,9 @@ const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => (
             ))}
         </ul>
     </div>
-
 )
 
-const EventTags = ({ tags}: { tags: string[] }) => (
+const EventTags = ({ tags }: { tags: string[] }) => (
     <div className="flex flex-row gap-1.5 flex-wrap">
         {tags.map((tag) => (
             <div className="pill" key={tag}>{tag}</div>
@@ -35,55 +35,60 @@ const EventTags = ({ tags}: { tags: string[] }) => (
 )
 
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+    'use cache'
+
+    cacheLife('hours');
     const { slug } = await params;
     const request = await fetch(`${BASE_URL}/api/events/${slug}`);
     const data = await request.json();
-    const { description, image, overview, date, time, location, mode, agenda, audience, tags, organizer } = data.event;
 
-    if (!description) return notFound();
+
+    const event: IEvent = data?.event;
+
+    if (!event || !event.description) return notFound();
+
+    const { description, image, overview, date, time, location, mode, agenda, audience, tags, organizer } = event;
+
 
     const bookings = 10;
 
     const similarEvents: IEvent[] = await getSimilarEventsBySlug(slug);
 
-    console.log({similarEvents})
 
     return (
         <section id="event">
             <div className="header">
-            <h1>Event Description</h1>
-            <p className="mt-2">{description}</p>
+                <h1>Event Description</h1>
+                <p className="mt-2">{description}</p>
             </div>
 
             <div className="details">
-                {/* Left Side - Event Content */}
+
                 <div className="content">
                     <Image src={image} alt="Event Banner" width={800} height={800} className="banner" />
 
-                <section className={"flex-col-gap-2"}>
-                    <h2>Overview</h2>
-                    <p>{overview}</p>
-                </section>
+                    <section className={"flex-col-gap-2"}>
+                        <h2>Overview</h2>
+                        <p>{overview}</p>
+                    </section>
 
                     <section className="flex-col-gap-2">
                         <h2>Event Details</h2>
-
                         <EventDetailItem icon="/icons/calendar.svg" alt="calendar" label={date} />
                         <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
                         <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location} />
                         <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
                         <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience} />
-
                     </section>
 
-                    <EventAgenda agendaItems={agenda} />
+                    {agenda && <EventAgenda agendaItems={Array.isArray(agenda) ? agenda : JSON.parse(agenda[0] || "[]")} />}
 
                     <section className="flex-col-gap-2">
                         <h2> About the Organizer</h2>
                         <p>{organizer}</p>
                     </section>
 
-                    <EventTags tags={JSON.parse(tags[0])} />
+                    {tags && <EventTags tags={Array.isArray(tags) ? tags : JSON.parse(tags[0] || "[]")} />}
                 </div>
 
                 {/* Right Side - Booking Form */}
@@ -94,11 +99,12 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> 
                             <p className="text-sm">
                                 Join {bookings} people who have already booked their spot!
                             </p>
-                        ): (
+                        ) : (
                             <p className="text-sm">Be the first to book your spot!</p>
                         )}
 
-                    <BookEvent />
+
+                        <BookEvent eventId={event._id.toString()} slug={event.slug} />
                     </div>
 
                     <div className="flex w-full flex-col gap-4 pt-20">
@@ -106,8 +112,8 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> 
                         <div className="events">
                             {similarEvents && similarEvents.length > 0 && similarEvents.map((similarEvent: IEvent) => (
                                 <EventCard
-                                    key={similarEvent.title} {...similarEvent}
-
+                                    key={similarEvent._id.toString()}
+                                    {...similarEvent}
                                 />
                             ))}
                         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import {cacheLife} from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const Page = async () =>
+const Page = async () => {
     'use cache';
 cacheLife('hours')
     const response = await fetch(`${BASE_URL}/api/events`);
